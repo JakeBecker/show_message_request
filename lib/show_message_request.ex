3b5e427f88dbc3e301eb66829b9d60148d584597
Jake Becker <jake@jakebecker.org>
2018-03-14 12:03:55 -0700

    Add show message request call

--- lib/show_message_request.ex
+++ lib/show_message_request.ex
@@ -1,18 +1,1 @@
-defmodule ShowMessageRequest do
-  @moduledoc """
-  Documentation for ShowMessageRequest.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ShowMessageRequest.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
-end
+Mix.shell().yes?("This request should have Yes/No buttons")
